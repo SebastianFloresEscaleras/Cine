3802142d30fe6e10d27473c6bd42da0ba8369368
Sebastian Alejandro Flores Escaleras <twinsebas899@gmail.com>
2026-07-05 19:57:23 -0500

    Add files via upload

---
+++ Cine.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fis.poo.cine;
+
+/**
+ *
+ * @author sebas
+ */
+public class Cine {
+
+    public static void main(String[] args) {
+        
+        
+        CineInterfaz cineui1 = new CineInterfaz();
+
+        cineui1.setVisible(true);
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+}
